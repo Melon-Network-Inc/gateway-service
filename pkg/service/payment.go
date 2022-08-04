587179333238228ffbbc46d73f44893cb2024bb5
Michael Zhou <michaelzhou@melonnetwork.io>
2022-08-04 01:29:48 -0700

    [Gateway] Support service routing for account and payment services.

--- pkg/service/payment.go
+++ pkg/service/payment.go
@@ -10,7 +10,10 @@ import (
 )
 
 type PaymentService interface {
-	HandleRequest(ctx *gin.Context)
+	HandlePostRequest(ctx *gin.Context)
+	HandleUpdateRequest(ctx *gin.Context)
+	HandleGetRequest(ctx *gin.Context)
+	HandleDeleteRequest(ctx *gin.Context)
 }
 
 type paymentService struct {}
@@ -19,18 +22,86 @@ func NewPaymentService() PaymentService {
 	return &paymentService{}
 }
 
-func (s *paymentService) HandleRequest(ctx *gin.Context) {
+func (s *paymentService) HandlePostRequest(ctx *gin.Context) {
 	client := resty.New()
 	userData := make(map[string]string)
-	userData["user"] = ctx.Value("username").(string)
-	userData["user_id"] = ctx.Value("user_id").(string)
+	if ctx.GetString("username") != "" && ctx.GetString("user_id") != "" {
+		userData["user"] = ctx.Value("username").(string)
+		userData["user_id"] = ctx.Value("user_id").(string)
+	}
 	var res []byte
 
 	_, err := client.R().
         SetBody(ctx.Request.Body).
 		SetHeaders(userData).
         SetResult(&res).
-        Post("http://localhost:7000/payment")
+        Post("http://localhost:7000" + ctx.Request.URL.String())
+
+    if err != nil {
+        log.Println("Payment Service: unable to connect PaymentService")
+        return
+    }
+	ctx.JSON(http.StatusOK, &res)
+}
+
+func (s *paymentService) HandleUpdateRequest(ctx *gin.Context) {
+	client := resty.New()
+	userData := make(map[string]string)
+	if ctx.GetString("username") != "" && ctx.GetString("user_id") != "" {
+		userData["user"] = ctx.Value("username").(string)
+		userData["user_id"] = ctx.Value("user_id").(string)
+	}
+	var res []byte
+
+	_, err := client.R().
+        SetBody(ctx.Request.Body).
+		SetHeaders(userData).
+        SetResult(&res).
+        Patch("http://localhost:7000" + ctx.Request.URL.String())
+
+    if err != nil {
+        log.Println("Payment Service: unable to connect PaymentService")
+        return
+    }
+	ctx.JSON(http.StatusOK, &res)
+}
+
+func (s *paymentService) HandleGetRequest(ctx *gin.Context) {
+	client := resty.New()
+	userData := make(map[string]string)
+	if ctx.GetString("username") != "" && ctx.GetString("user_id") != "" {
+		userData["user"] = ctx.Value("username").(string)
+		userData["user_id"] = ctx.Value("user_id").(string)
+	}
+	var res []byte
+
+	_, err := client.R().
+        SetBody(ctx.Request.Body).
+		SetHeaders(userData).
+        SetResult(&res).
+        Get("http://localhost:7000" + ctx.Request.URL.String())
+
+    if err != nil {
+        log.Println("Payment Service: unable to connect PaymentService")
+        return
+    }
+	ctx.JSON(http.StatusOK, &res)
+}
+
+func (s *paymentService) HandleDeleteRequest(ctx *gin.Context) {
+	client := resty.New()
+	userData := make(map[string]string)
+	if ctx.GetString("username") != "" && ctx.GetString("user_id") != "" {
+		userData["user"] = ctx.Value("username").(string)
+		userData["user_id"] = ctx.Value("user_id").(string)
+	}
+	var res []byte
+
+	_, err := client.R().
+        SetBody(ctx.Request.Body).
+		SetHeaders(userData).
+        SetResult(&res).
+        Delete("http://localhost:7000" + ctx.Request.URL.String())
 
     if err != nil {
         log.Println("Payment Service: unable to connect PaymentService")
